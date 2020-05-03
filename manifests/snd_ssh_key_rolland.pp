cf731ec194e205d3a47832cd838a5440f5a84e67
StarJun26 <rollandfotsing@gmail.com>
2020-05-03 20:16:04 +0000

    Trying to add two ssh keys to the same user via puppet

--- manifests/snd_ssh_key_rolland.pp
+++ manifests/snd_ssh_key_rolland.pp
@@ -1,8 +1,5 @@
 ssh_authorized_key { 'rollandfotsing@ymail.com':
 user => 'rolland',
 type => 'ssh-rsa',
-key => 'AAAAB3NzaC1yc2EAAAABIwAAAIEA3ATqENg+GWACa2
-BzeqTdGnJhNoBer8x6pfWkzNzeM8Zx7/2Tf2pl7kHdbsiTXEUawq
-zXZQtZzt/j3Oya+PZjcRpWNRzprSmd2UxEEPTqDw9LqY5S2B8og/
-NyzWaIYPsKoatcgC7VgYHplcTbzEhGu8BsoEVBGYu3IRy5RkAcZik=',
+key => 'AAAAB3NzaC1yc2EAAAABIwAAAIEA3ATqENg+GWACa2BzeqTdGnJhNoBer8x6pfWkzNzeM8Zx7/2Tf2pl7kHdbsiTXEUawqzXZQtZzt/j3Oya+PZjcRpWNRzprSmd2UxEEPTqDw9LqY5S2B8og/NyzWaIYPsKoatcgC7VgYHplcTbzEhGu8BsoEVBGYu3IRy5RkAcZik=',
 }
